0c72f3767df8c6912597000ef25b5f7f30c32525
oahidalgo <ohgovilla@gmail.com>
2023-10-28 20:57:31 -0600

    ADD - Initial commit - CRUD of tasks

---
+++ src/updateTask.js
@@ -0,0 +1,35 @@
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+const updateTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const { id } = event.pathParameters;
+
+  const { done, title, description } = JSON.parse(event.body);
+
+  await dynamodb
+    .update({
+      TableName: "TaskTable",
+      Key: { id },
+      UpdateExpression:
+        "set done = :done, title = :title, description = :description",
+      ExpressionAttributeValues: {
+        ":done": done,
+        ":title": title,
+        ":description": description,
+      },
+      ReturnValues: "ALL_NEW",
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "task updated",
+    }),
+  };
+};
+
+module.exports = {
+  updateTask,
+};
